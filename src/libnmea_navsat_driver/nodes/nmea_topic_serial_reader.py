40f0a5bc28958ea19e19cbf81aa455085bf7588a
jean.laneurit <jean.laneurit@inrae.fr>
2023-06-06 15:07:38 +0200

    frame decoding in using utf8 format

--- src/libnmea_navsat_driver/nodes/nmea_topic_serial_reader.py
+++ src/libnmea_navsat_driver/nodes/nmea_topic_serial_reader.py
@@ -56,11 +56,10 @@ def main(args=None):
         try:
             while rclpy.ok():
                 data = GPS.readline().strip()
-
                 sentence = Sentence()
                 sentence.header.stamp = driver.get_clock().now().to_msg()
                 sentence.header.frame_id = frame_id
-                sentence.sentence = data
+                sentence.sentence = data.decode('utf-8')
                 nmea_pub.publish(sentence)
 
         except Exception as e:
